646a567eb68c3abd046c04be55a1660f7d968aab
VlaP <72850129+ykGoose@users.noreply.github.com>
2023-07-31 14:54:39 +0900

    comp

--- traffic/ViewController.swift
+++ traffic/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var yellowLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    
+    @IBOutlet var lightButton: UIButton!
+    
+    enum CurrentLight {
+        case red, yellow, green
+    }
+    
+    var currentLight = CurrentLight.green
+    let on = 1.0
+    let off = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configUI()
     }
 
+    @IBAction func lightChanged() {
+        
+        switch currentLight {
+        case .red:
+            redLabel.alpha = off
+            yellowLabel.alpha = on
+            currentLight = .yellow
+        case .yellow:
+            yellowLabel.alpha = off
+            greenLabel.alpha = on
+            currentLight = .green
+        case .green:
+            greenLabel.alpha = off
+            redLabel.alpha = on
+            currentLight = .red
+        }
+    }
+    
 
+    
+    func configUI() {
+        
+        redLabel.backgroundColor = .red
+        yellowLabel.backgroundColor = .yellow
+        greenLabel.backgroundColor = .green
+        redLabel.alpha = off
+        yellowLabel.alpha = off
+        greenLabel.alpha = off
+
+        redLabel.layer.cornerRadius = (redLabel.frame.size.width / 2)
+        redLabel.clipsToBounds = true
+        yellowLabel.layer.cornerRadius = (yellowLabel.frame.size.width / 2)
+        yellowLabel.clipsToBounds = true
+        greenLabel.layer.cornerRadius = (greenLabel.frame.size.width / 2)
+        greenLabel.clipsToBounds = true
+        
+        lightButton.setTitle("Start", for: .normal)
+        
+    }
+    
+    
 }
 
